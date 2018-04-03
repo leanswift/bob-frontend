b29e6e8035cd40ab238ef19a820ef5d9052c47bd
Shyam Sathyanathan <shyam.sathyanathan@leanswift.com>
2018-04-03 16:28:06 +0530

    Fixed file download support for webapp

--- src/app/download-and-install/download-and-install.component.ts
+++ src/app/download-and-install/download-and-install.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import * as arrayBufferToBuffer from 'arraybuffer-to-buffer';
+import { saveAs } from 'file-saver';
 
 import { BobService, ProgressCallback } from '../service/bob.service';
 
@@ -10,32 +11,32 @@ import { BobService, ProgressCallback } from '../service/bob.service';
 })
 export class DownloadAndInstallComponent implements OnInit {
 
-    fs: any;
+    // fs: any;
     eLinkVersion: string;
     progressCallback: ProgressCallback;
 
     constructor(private bobService: BobService, private router: Router, private route: ActivatedRoute) {}
 
     public ngOnInit() {
-        this.fs = window['fs'];
+        // this.fs = window['fs'];
         this.route.params.subscribe(params => {
             this.eLinkVersion = params['eLinkVersion'];
         });
 
         this.progressCallback = new ProgressCallbackImpl();
 
-        console.log(window['fs']);
-
         this.bobService.downloadELink(this.eLinkVersion, this.progressCallback)
             .subscribe(data => {
                 console.log(data);
-                let content = arrayBufferToBuffer(data._body);
-                console.log(content);
-                this.fs.writeFile(this.eLinkVersion + '.war', content, (err) => {
-                    if(err !== null) {
-                        console.log(err);
-                    }
-                });
+                // let content = arrayBufferToBuffer(data._body);
+                // console.log(content);
+                // this.fs.writeFile(this.eLinkVersion + '.war', content, (err) => {
+                //     if(err !== null) {
+                //         console.log(err);
+                //     }
+                // });
+                const blob = data.blob();
+                saveAs(blob, this.eLinkVersion + '.war');
             });
     }
 
